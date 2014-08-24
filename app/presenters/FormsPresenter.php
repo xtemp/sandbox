baef7bc350e72ccc054b8e563c91f239a411ad0e
Radek Burget <burgetr@fit.vutbr.cz>
2014-08-24 23:37:35 +0200

    Dynamic forms

--- app/presenters/FormsPresenter.php
+++ app/presenters/FormsPresenter.php
@@ -19,6 +19,17 @@ class FormsPresenter extends BasePresenter
 	{
 	}
 
+	public function renderDynamic()
+	{
+		$carList = new \Nette\ArrayList();
+		$carList[] = new Car("red", "1990", "Audi", "Hanz", "172400");
+		$carList[] = new Car("blue", "1995", "Audi", "Peter", "159400");
+		$carList[] = new Car("green", "1980", "Ford", "Lukas", "117400");
+		$carList[] = new Car("yellow", "2000", "Skoda", "Quido", "576400");
+		$carList[] = new Car("pink", "2010", "BMW", "Frodo", "16300");
+		$this->template->carList = $carList;
+	}
+	
 	public function formSubmitted()
 	{
 		//echo "NAZDAR!";
